e62e71f58b511e9b72f9593b81e250afe5dbfe11
Nishant1195 <nishant8830875196@gmail.com>
2025-08-06 22:42:07 +0530

    Login Page Done

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,4 @@
+import bcrypt from "bcryptjs";
 import User from "../model/user-model.js";
 import generateToken from "../model/user-model.js"
 
@@ -21,10 +22,35 @@ export const registration = async (req, res) => {
         }
         
         const result = await User.create({username, email, phoneno, password})
-        res.status(201).json({msg: result, token: await result.generateToken(), userID: result._id.toString()});
+        res.status(201).json({msg: "Registration Unsuccessful", token: await result.generateToken(), userID: result._id.toString()});
 
     } catch (error) {
         console.log(error);
         res.status(500).json({msg:"Internal Server Error"});
+    }
+}
+
+export const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+
+        const userExists = await User.findOne({email});
+
+        if(!userExists){
+            return res.status(400).json({msg: "Invalide Credentials"});
+        }
+
+        const user = await bcrypt.compare(password, userExists.password);
+
+        if(user){
+            return res.status(201).json({msg: "Login Successful", token: await userExists.generateToken(), userID: userExists._id.toString()});
+        }else{
+            return res.status(401).json({msg: "Invalide Credentials"});
+        }
+
+
+    } catch (error) {
+        console.log("Error at Login Route", error);
+        res.status(500).json({msg: "Internal Server Error"})
     }
 }
--- server/router/auth-route.js
+++ server/router/auth-route.js
@@ -1,10 +1,12 @@
 import express from "express";
-import {home, registration} from "../controllers/auth-controller.js"
+import {home, registration, login} from "../controllers/auth-controller.js"
 
 const router = express.Router();
 
 router.route("/").get(home);
 
 router.route("/registration").post(registration);
+
+router.route("/login").post(login);
 
 export default router
