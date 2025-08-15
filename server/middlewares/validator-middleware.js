9704ee112458d966420c1988489c82d95f0d6e08
Nishant1195 <nishant8830875196@gmail.com>
2025-08-16 00:05:04 +0530

    Error Middleware added

--- server/middlewares/validator-middleware.js
+++ server/middlewares/validator-middleware.js
@@ -6,24 +6,15 @@ const validate = (schema) => async (req, res, next) => {
             req.body = parseBody;
             next();
         }  catch (err) {
-        console.log("Error caught:", err); // Debug log
-        console.log("Error type:", err.constructor.name); // Debug log
-        
-        if (err instanceof ZodError) {
-            // Additional safety check
-            const errorMessage = err.errors && err.errors.length > 0 
-                ? err.errors[0].message 
-                : "Invalid input";
-            
-            res.status(400).json({ 
-                msg: errorMessage,
-                // Optional: include all errors for debugging
-                errors: err.errors 
+            if (err instanceof ZodError) {
+            return next({
+                status: 422,
+                message: "Please fill the data properly",
+                extraDetails: err.errors[0]?.message
             });
-        } else {
-            res.status(500).json({ msg: "Something went wrong" });
-        }
-    }
+            }
+            next(err); // Pass other errors to error middleware
+  }
 }
 
 export default validate
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import router from "./router/auth-route.js";
 import connectDB from "./utils/db.js";
 import dotenv from "dotenv";
+import errorMiddleware from "./middlewares/error-middleware.js";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ app.use(express.json());
 
 app.use("/api/auth", router);
 
+app.use(errorMiddleware);
 connectDB().then(() => {
     app.listen(PORT, () => {
         console.log(`Listening to Port: ${PORT}`)
